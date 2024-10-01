f81801555e2b80b0210b22b0d978c660302441b7
Maximiliano Varas <maximiliano.varas@tifon.cl>
2024-10-01 17:01:43 -0300

    se añade paquete lucide

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+// Importa el paquete de lucide
+import { createIcons, icons } from 'lucide';
+
+createIcons({
+  icons,
+});
