8679d57a10be256c476e468675a49df0e5278f85
Maximiliano Varas <maximiliano.varas@tifon.cl>
2024-10-22 23:08:16 -0300

    Updates

--- index.18dbc454.js
+++ index.18dbc454.js
@@ -595,7 +595,7 @@ document.getElementById("whatsappForm").addEventListener("submit", function(e) {
     e.preventDefault();
     const nombre = document.getElementById("nombre").value;
     const mensaje = document.getElementById("mensaje").value;
-    const telefono = "+56994166766"; // Reemplaza esto con tu número de WhatsApp
+    const telefono = "+56994166766";
     const textoCompleto = `Hola, soy ${nombre}. ${mensaje}`;
     const encodedText = encodeURIComponent(textoCompleto);
     window.open(`https://api.whatsapp.com/send?phone=${telefono}&text=${encodedText}`, "_blank");
